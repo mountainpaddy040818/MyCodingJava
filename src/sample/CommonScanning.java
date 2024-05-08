31e1e290edafcccfe3bc4163652f01f68824a963
Akihiro <mountainpaddy040818@gmail.com>
2024-05-08 23:11:36 +0900

    標準入力、出力

---
+++ src/sample/CommonScanning.java
@@ -0,0 +1,14 @@
+package sample;
+
+import java.util.Scanner;
+
+public class CommonScanning {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		System.out.println(number);
+	}
+
+}
