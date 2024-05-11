ee41bc52704a29f59e4ef976dcc937d36ca28265
Akihiro <mountainpaddy040818@gmail.com>
2024-05-11 12:27:58 +0900

    コメントアウト記述

--- src/sample/JavaClass.java
+++ src/sample/JavaClass.java
@@ -4,9 +4,9 @@ public class JavaClass {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-//		iは一から始まり、20までカウント、１ずつ増加していく
+//		iは1から10までカウント、1ずつ上昇し","区切りで表示
 		for (int i = 1; i <= 10; i++)
-			if(i < 20) {
+			if(i < 10) {
 				System.out.print(i + ", ");
 			} else {
 				System.out.print(i);
