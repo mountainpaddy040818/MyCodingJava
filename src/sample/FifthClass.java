8a5a28b23aa12b9e3fa43eedcee0bef728fece40
Akihiro <mountainpaddy040818@gmail.com>
2024-05-14 11:46:33 +0900

    Javaのコード理解

--- src/sample/FifthClass.java
+++ src/sample/FifthClass.java
@@ -5,13 +5,13 @@ public class FifthClass {
         int[] array = new int[20];
         // 配列に値を代入
         for (int i = 0; i < 20; i++) {
-            array[i] = i * 5;
+            array[i] = i;
         }
 
         // [0]〜[10]の奇数をカンマ区切りで表示
         for (int i = 0; i <= 10; i++) {
             if (array[i] % 2 != 0) { // 奇数かどうか判定
-                if (i < 10) {
+                if (i < 9) {
                     System.out.print(array[i] + ", ");
                 } else {
                     System.out.print(array[i]);
@@ -25,7 +25,7 @@ public class FifthClass {
 
         // [11]〜[19]の偶数をカンマ区切りで表示
         for (int i = 11; i < 20; i++) {
-            if (array[i] % 2 == 0) { // 偶数かどうか判定
+            if (array[i] % 2 != 0) { // 偶数かどうか判定
                 if (i < 19) {
                     System.out.print(array[i] + ", ");
                 } else {
