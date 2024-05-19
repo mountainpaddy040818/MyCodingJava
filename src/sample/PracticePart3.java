9f06c3863997cfd0beaa2e4e1fa4fdb34db9a7dc
Akihiro <mountainpaddy040818@gmail.com>
2024-05-19 22:40:17 +0900

    繰り返し文練習

--- src/sample/PracticePart3.java
+++ src/sample/PracticePart3.java
@@ -4,8 +4,8 @@ public class PracticePart3 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for (int i = 1; i <= 100; i++) {
-			if (i < 100) {
+		for(int i = 1; i <= 100; i++) {
+			if(i < 100) {
 				System.out.print(i + ", ");
 			} else {
 				System.out.print(i);
