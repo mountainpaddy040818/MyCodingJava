4ff9677ca78d15a0b3c5aa629c7ac072c7c5f56c
Akihiro <mountainpaddy040818@gmail.com>
2024-05-21 21:01:02 +0900

    同じ数字同士の掛け算結果出力

--- src/sample/CommonScanning.java
+++ src/sample/CommonScanning.java
@@ -7,8 +7,9 @@ public class CommonScanning {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		Scanner scan = new Scanner(System.in);
-		int number = scan.nextInt();
-		System.out.println(number);
+		System.out.println("数字同士の掛け算結果が出力されます、数字を入力してください");
+		int number1 = scan.nextInt();
+		System.out.println(number1 * number1);
 	}
 
 }
