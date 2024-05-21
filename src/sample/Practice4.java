165b902c128084bb230fe0484b61cc8a889d7405
Akihiro <mountainpaddy040818@gmail.com>
2024-05-21 20:29:44 +0900

    配列作成の練習

---
+++ src/sample/Practice4.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Practice4 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] array = {1, 2, 3, 4, 100};
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		System.out.println("配列の合計は");
+		System.out.println(sum);
+	}
+
+}
