79c5bc505cb5bca32c6c93cd530bed754ba8381b
Akihiro <mountainpaddy040818@gmail.com>
2024-05-22 15:12:26 +0900

    標準入力、標準出力

---
+++ src/sample/Practice7.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Practice7 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+        int line = scan.nextInt();
+        for (int n = 0; n < line; n++) {
+            String s = scan.next();
+            System.out.println(s);
+        }
+	}
+
+}
