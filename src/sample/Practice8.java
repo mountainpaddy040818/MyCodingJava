96904a95638b8de260bd6f399cda302b61f55cf3
Akihiro <mountainpaddy040818@gmail.com>
2024-05-25 23:32:46 +0900

    FizzBuzzプログラム作成

--- src/sample/Practice8.java
+++ src/sample/Practice8.java
@@ -1,10 +1,24 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Practice8 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		for (int i = 1; i <= 100; i++) {
+			if (i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
