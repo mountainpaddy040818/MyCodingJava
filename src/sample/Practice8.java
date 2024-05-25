8d1f3805c3188c6579fe769454a30dbfb5d2a0ba
Akihiro <mountainpaddy040818@gmail.com>
2024-05-25 23:34:21 +0900

    FizzBuzz修正

--- src/sample/Practice8.java
+++ src/sample/Practice8.java
@@ -1,13 +1,11 @@
 package sample;
 
-import java.util.Scanner;
-
 public class Practice8 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+//		Scanner scan = new Scanner(System.in);
+//		int n = scan.nextInt();
 		for (int i = 1; i <= 100; i++) {
 			if (i % 15 == 0) {
 				System.out.println("FizzBuzz");
