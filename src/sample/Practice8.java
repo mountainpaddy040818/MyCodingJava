2eba47e7c4565d9dd2c91598cf6c42b3ac87ff6c
Akihiro <mountainpaddy040818@gmail.com>
2024-05-25 23:38:41 +0900

    FizzBuzzコメントアウト表示

--- src/sample/Practice8.java
+++ src/sample/Practice8.java
@@ -3,16 +3,18 @@ package sample;
 public class Practice8 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-//		Scanner scan = new Scanner(System.in);
-//		int n = scan.nextInt();
-		for (int i = 1; i <= 100; i++) {
+//		変数iは1~1000まで出力される
+		for (int i = 1; i <= 1000; i++) {
+//		3と5で割り切れる数値の場合は"FizzBuzz"と出力	
 			if (i % 15 == 0) {
 				System.out.println("FizzBuzz");
+//		3のみで割り切れる場合は"Fizz"と出力		
 			} else if (i % 3 == 0) {
 				System.out.println("Fizz");
+//		5のみで割り切れる場合は"Buzz"と出力		
 			} else if (i % 5 == 0) {
 				System.out.println("Buzz");
+//		それ以外の数値はそのまま出力		
 			} else {
 				System.out.println(i);
 			}
