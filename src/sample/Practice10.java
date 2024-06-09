41709cd8abd403eedc69f34499156d3211e38752
Akihiro <mountainpaddy040818@gmail.com>
2024-06-09 16:07:34 +0900

    西暦と平成の対応表

---
+++ src/sample/Practice10.java
@@ -0,0 +1,22 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Practice10 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Scanner scan = new Scanner(System.in);
+		System.out.println("西暦を入力してください");
+		int seireki = scan.nextInt();
+		System.out.println("出力する年数を入力してください");
+		int kikan = scan.nextInt();
+		for(int i = 0; i < kikan; i++) {
+			int seireki2 = seireki + i;
+			System.out.print("西暦" + seireki2 + "年は、");
+			int heisei = seireki2 - 1988;
+			System.out.println("平成" + heisei + "年です。");
+		}
+	}
+
+}
