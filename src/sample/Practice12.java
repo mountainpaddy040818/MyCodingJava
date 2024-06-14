c0b1b9e59a69f5edf9e59c36a68816413aa3bbe6
Akihiro <mountainpaddy040818@gmail.com>
2024-06-14 18:29:13 +0900

    配列の生成

--- src/sample/Practice12.java
+++ src/sample/Practice12.java
@@ -3,8 +3,11 @@ package sample;
 public class Practice12 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		
+//		 TODO 自動生成されたメソッド・スタブ
+		String[] food = {"おにぎり", "カレー", "唐揚げ"};
+		for(int i = 0; i < food.length; i++) {
+			System.out.println(food[i]);
+		}
 	}
 
 }
