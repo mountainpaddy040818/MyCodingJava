b8431db203bf4aa54f514362dccf299858f99391
Akihiro <mountainpaddy040818@gmail.com>
2024-06-19 16:34:38 +0900

    課題1,2

--- src/sample/Seiseki.java
+++ src/sample/Seiseki.java
@@ -1,43 +1,64 @@
 package sample;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
-//各教科の点数を入力して、合計と平均点を求めるクラス
+/**
+ * 国語,数学,社会,理科,英語5教科の点数を入力する。
+ * その際、整数以外や未入力、 101点以上の点数であった場合、メッセージを出力して再入力を促す。
+ * 全教科の点数が入力された後、その合計点と平均点の算出結果を画面に出力する。 
+ * 平均点は小数点第一位以下まで算出する。
+ */
 public class Seiseki {
-
+	/**
+	 *  定義した定数配列（各科目の名称）をmainメソッド内で使用する。
+	 */
+	public static final List<String> KAMOKU = 
+			Collections.unmodifiableList(Arrays.asList("国語","数学","社会","理科","英語"));
+	/**
+	 * mainメソッド内で各科目の合計点、平均点を計算し算出する。
+	 * 入力された値が0~100以外の場合は無効とする。
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String[] kamoku = {"国語", "数学", "社会", "理科", "英語"};
-		int[] seiseki = new int[5];
+		// 5科目の点数を格納するための変数配列
+		int[] seiseki1 = new int[5];
+		// 5科目の合計点数を算出するための変数
 		int gokei = 0;
-		for(int i = 0; i < kamoku.length; i++) {
-			System.out.println(kamoku[i] + "の点数を入力してください。");	
-//			while(true)で入力された値がint型か否か確認
-			while(true) {
+		// 配列から各科目を取り出し、ループで点数を格納していく
+		for(int i = 0; i < KAMOKU.size(); i++) {
+			System.out.println(KAMOKU.get(i) + "の点数を入力してください。");
+			// 入力された値が0以上か否か確認
+			while(seiseki1[i] >= 0) {
+				// 入力された値に対して、0~100点の中で処理がされているか
 				if(scan.hasNextInt()) {
-//					各教科の点数を入力してください
-					seiseki[i] = scan.nextInt();
-					if(seiseki[i] >= 0 && seiseki[i] <= 100) {
+					// 各教科の点数を入力してください
+					seiseki1[i] = scan.nextInt();
+					if(seiseki1[i] >= 0 && seiseki1[i] <= 100) {
 						break;
-					} else if(seiseki[i] < 0) {
+					} else if(seiseki1[i] < 0) {
 						System.out.println("マイナス点です。もう一度入力してください。");
-					} else if(seiseki[i] > 100) {
+					} else if(seiseki1[i] > 100) {
 						System.out.println("101点以上です。もう一度入力してください。");
-					} else {
-						System.out.println("もう一度入力してください。");
 					}
 				} else {
-					System.out.println("自然数ではありません。もう一度入力してください。");
+					System.out.println("非負整数の中でもう一度入力してください。");
 					scan.next();
 				}
-			}
-//			seiseki[i]を順番に足して合計算出
-			gokei += seiseki[i];
-//			heikin変数をdouble型とすることで小数点まで算出
-			double heikin = gokei / seiseki[i];
+			}	
+			// 配列の変数を通常の変数に変更
+			int seiseki2 = seiseki1[i];
+			
+			// seiseki2を順番に足して各教科の合計算出
+			gokei += seiseki2;
 		}
-		double heikin = gokei / 5.0;
-		System.out.println("合計：" + gokei + "点です。");
-		System.out.println("平均点：" + heikin + "点です。");
+		// 5で割るという意味合いで命名しました。
+		double DevideByFive = 5.0;
+		// heikin変数をdouble型とすることで小数点第一位まで算出
+		double heikin = gokei / DevideByFive;
+		System.out.println("合計：" + gokei + "点です。" + "平均：" + heikin + "点です。");
 	}
 }
--- src/sample/SortSample.java
+++ src/sample/SortSample.java
@@ -1,36 +1,46 @@
 package sample;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-//入力された5つの引数をソートし、先頭からX番目の値を出力するクラス
+/**
+ * 入力された5つの引数をソートし、先頭からX番目の値を出力するクラス
+ * ※出力対象となる番号は6番目の引数を使用する。
+ */
 public class SortSample {
-
+	/**
+	 * 6番目に入力された値が数字の場合は対応する配列から値を出力
+	 * 6番目に数字以外または配列数よりも長い数字などが入力された場合はエラー文を返す。
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] SortList = new String[5];
 		System.out.println("値を6つ入力してください。");
-//		入力された値をスペース区切りで受け取る
+		// 入力された値をスペース区切りで受け取る
 		SortList = scan.nextLine().split(" ");
-//		配列の長さが6以外の場合、エラーを出力
-		int x = 0;
+		// 配列の長さが6以外の場合、エラーを出力
+		int x = 1;
 		if(SortList.length != 6) {
 			System.out.println("設定された引数の数が正しくありません。確認の上再度実行してください。");
 			return;
 		} else {
 			try {
-//				6番目に入力した数値に対応する文字を配列SortListから取り出す
+				// 6番目に入力した数値に対応する文字を配列から取り出す
 				x = Integer.parseInt(SortList[5]);
 			} catch (NumberFormatException e) {
 				System.out.println("6番目の引数は数字で設定してください。");
 				scan.next();
 			}
 		}
-//		決められた配列の中で値を判別するため、0~5以外を無効とする。
-		if(x < 0 || x > 6) {
-			System.out.println("入力する整数は0~5の中で指定してください。");
+		// 決められた配列の中で値を判別するため、1~6以外を無効とする。
+		if(x < 1 || x > 7) {
+			System.out.println("入力する整数は1~6の中で指定してください。");
 			scan.next();
 		}
-//		SortListのx番目を出力
+		// 配列のx番目を出力
 		System.out.println(SortList[5] + "番目の値は" + SortList[x] + "です。");
+		Arrays.sort(SortList);
+		System.out.println(Arrays.toString(SortList));
 	}    
 }
