67e0f0d7d3ec33a97c793832b49344f8428795ee
Akihiro <mountainpaddy040818@gmail.com>
2024-06-21 15:17:40 +0900

    課題３

--- src/sample/OutPutEmpInfo.java
+++ src/sample/OutPutEmpInfo.java
@@ -1,28 +1,55 @@
 package sample;
 
-//入力された社員名及び社員番号を表示するクラス
+/**
+ * 入力された社員名及び社員番号を表示するクラス
+ */
 public class OutPutEmpInfo {
 	
 	/**
-	 * 社員番号を入力されなかった場合のデフォルト値
+	 * 社員番号が引数に渡されなかった場合のデフォルト値
 	 */
 	private static final int DefEmpNumber = 999999999;
 	
 	/**
-	 * 
-	 * @param args 実行時の引数。引数は不要。すべての入力は実行時にコンソールから行われる。
+	 * <p>
+	 * プログラムのメインメソッド
+	 * 入力された引数を処理するためにOutPutEmpInfoクラスのインスタンスを作成し、startメソッドを呼び出す。
+	 * </p>
+	 * @param args プログラム実行時に渡される引数:
+	 *             - args[0]: 社員名（必須）
+	 *             - args[1]: 社員番号（省略可能）
 	 */
 	public static void main(String[] args) {
 		OutPutEmpInfo empInfo = new OutPutEmpInfo();
-		empInfo.start();
-	}
-	
-	private void start() {
-		
+		empInfo.start(args);
 	}
 	
 	/**
-	 * 
+	 * 引数を受け取り、mainメソッドに渡す。
+	 * @param inputs プログラム実行時に渡される引数:
+	 * 				- args[0]: 社員名（必須）
+	 *              - args[1]: 社員番号（省略可能）
+	 */
+	private void start(String[] inputs) {
+		switch (inputs.length) {
+			case 0:
+				System.out.println("入力値が設定されていません。引数を確認の上再度実行してください。");
+				return;
+			case 1:
+				OutPutEmpInfo(inputs[0]);
+				return;
+			case 2:
+				OutPutEmpInfo(inputs[0], inputs[1]);
+				return;
+			default:
+				System.out.println("引数が多く設定されているため、余計な引数を無視します。");
+				OutPutEmpInfo(inputs[0], inputs[1]);
+				return;
+		}
+	}
+	
+	/**
+	 * 名前のみ引数に渡された場合の処理、社員番号はデフォルト値とする。
 	 * @param name 引数は社員の名前
 	 */
 	private void OutPutEmpInfo(String name) {
@@ -32,7 +59,7 @@ public class OutPutEmpInfo {
 	}
 	
 	/**
-	 * 
+	 * 名前と社員番号が渡された場合の処理
 	 * @param name 引数は名前
 	 * @param EmpNumber 引数は社員番号
 	 */
