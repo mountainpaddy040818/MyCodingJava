2b245dce8b690360ed55c784d0ba451b4a76ba40
Akihiro <mountainpaddy040818@gmail.com>
2024-06-27 17:40:53 +0900

    課題5

--- src/sample/MondayCalendar.java
+++ src/sample/MondayCalendar.java
@@ -1,6 +1,6 @@
 package sample;
-import java.time.LocalDate;
-import java.time.YearMonth;
+
+import java.util.Calendar;
 
 /**
  * 月曜日から始まるカレンダーを作成する。
@@ -12,17 +12,14 @@ import java.time.YearMonth;
  * 入力が合った場合は、以下のチェックを行う。
  */
 public class MondayCalendar {
+	// 曜日
+	private static final String[] WEEK = {"月", "火", "水", "木", "金", "土", "日"};
 	// 年
 	private int year = 0;
 	// 月
 	private int month = 0;
-	
-	/**
-	 * デフォルト
-	 */
-	public MondayCalendar() {
-		
-	}
+	// カレンダー
+	private Calendar calendar;
 	
 	/**
 	 * 年と月が与えられた場合の処理
@@ -30,96 +27,109 @@ public class MondayCalendar {
 	 * @param year 年
 	 * @param month 月
 	 */
-	public void MondayCalendar(int year, int month) {
-		this.year = year;
-		this.month = month;
+	public MondayCalendar() {
+		this.calendar = Calendar.getInstance();
+		this.init();
 	}
 	
 	/**
-	 * メインメソッド
+	 * 引数が与えられた場合
 	 * 
-	 * @param args 西暦と月
+	 * @param year 年
+	 * @param month 月
+	 */
+	private void setCalendar(int year, int month) {
+		this.year = year;
+		this.month = month;
+		this.calendar.set(Calendar.YEAR, year);
+		this.calendar.set(Calendar.MONTH, month - 1);
+	}
+	
+	/**
+	 * 初期化処理
+	 */
+	private void init() {
+		this.year = this.calendar.get(Calendar.YEAR);
+		this.month = this.calendar.get(Calendar.MONTH) + 1;
+	}
+	
+	/**
+	 * 年、月の表示
+	 */
+	private void header() {
+		System.out.printf("　　　　" + "〜　%d年　%d月　〜　%n", this.year, this.month);
+		System.out.print(" ");
+		// カレンダー上部に曜日表示
+		for(String week : WEEK) {
+			System.out.print("  " + week);
+		}
+		System.out.println();
+	}
+	
+	/**
+	 * カレンダーのエントリーポイント
+	 * 
+	 * @param args 年、月
 	 */
 	public static void main(String[] args) {
 		MondayCalendar calendar = new MondayCalendar();
-		calendar.display(args);
-	}
-	
-	/**
-	 * 引数の数に応じてメッセージ出力
-	 * 
-	 * @param args 西暦と月
-	 */
-	private void display(String[] args) {
-		try {
-			// 
-			if(args.length == 0) {
-				// 
-				LocalDate now = LocalDate.now();
-				year = now.getYear();
-				month = now.getMonthValue();
-				//
-			} else if (args.length == 2) {
-				// 
-				try {
-					year = Integer.parseInt(args[0]);
-					if (year < 1900) {
-						System.out.println("西暦は1901年以降で入力してください。");
-						return;
-					}
-				} catch(NumberFormatException e) {
-					System.out.println("数字を入力してください。");
+		// 引数が二つ与えられた場合
+		if(args.length == 2) {
+			try {
+				// 整数型へ変換
+				int year = Integer.parseInt(args[0]);
+				// 整数型へ変換
+				int month = Integer.parseInt(args[1]);
+				// 1900以下はエラー
+				if(year <= 1900) {
+					System.out.println("年は1900以降で入力してください。");
 					return;
 				}
-				// 
-				try {
-					month = Integer.parseInt(args[1]);
-					if (month < 1 || month > 12) {
-						System.out.println("1~12の数字を入力してください。");
-						return;
-					}
-				} catch(NumberFormatException e) {
-					System.out.println("数字を入力してください。");
+				// 0以下または13以上はエラー
+				if(month < 1 || month > 12) {
+					System.out.println("月は1~12の数値を入力してください。");
 					return;
 				}
-				// 
-			} else {
-				System.out.println("引数の数を確認してください。");
+				calendar.setCalendar(year, month);
+			// 半角、全角数字以外はエラー
+			} catch(NumberFormatException e) {
+				System.out.println("半角または全角数字で入力してください。");
 				return;
 			}
-			// 
-		} catch(NumberFormatException e) {
-			System.out.println("実行エラー。入力可能な値は数字のみです。");
+		// 引数が一つまたは三つ以上の場合はエラー
+		} else if(args.length == 1 || args.length >= 3) {
+			System.out.println("引数に誤りがあります、確認してください。");
+			return;
 		}
+		calendar.display();
 	}
 	
 	/**
 	 * カレンダー表示
 	 * 
-	 * @param year 年
-	 * @param month 月
+	 * @param args 年、月
 	 */
-	public void calendar(int year, int month) {
-		YearMonth yearMonth = YearMonth.of(year, month);
-		LocalDate firstDayOfMonth = yearMonth.atDay(1);
-		
-		System.out.printf("~ %d年 %d月 ~%n", year, month);
-		System.out.println("月 火 水 木 金 土 日");
-		// 毎月1日
-		int firstDayOfWeek = firstDayOfMonth.getDayOfWeek().getValue();
-		// 
-		int adjustment = (firstDayOfWeek == 7) ? 0 : firstDayOfWeek;
-		
-		for(int i = 0; i < adjustment; i++) {
-			System.out.print("　　");
-		}
-		
-		for(int day = 1; day <= yearMonth.lengthOfMonth(); day++) {
-			System.out.printf("%2d", day);
-			if((day + adjustment - 1) % 7 == 0) {
+	private void display() {
+		this.calendar.set(Calendar.DATE, 1);
+		// カレンダーの余白部分
+		int beforeBlank = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+		// 一月が30日までか、31日までか。
+		int daysCount = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+		this.header();
+		// 余白と日数分をループ処理
+		for(int i = 0; i < beforeBlank + daysCount; i++) {
+			String string = " ";
+			// 空白を空ける
+			if(i >= beforeBlank) {
+				int date = i + 1 - beforeBlank;
+				string = String.valueOf(date);
+			}
+			System.out.printf("%4s", string);
+			// 7日表示したら改行
+			if((i + 1) % 7 == 0) {
 				System.out.println();
 			}
 		}
 		System.out.println();
 	}
-}
+}	
